f9cb84f07746b4af9e1c25b8684f7fa03e005a58
maluki65 <brayanmaluki@gmail.com>
2025-08-06 10:43:04 +0300

    Added cart to localStorage

--- data/Cart.js
+++ data/Cart.js
@@ -1,4 +1,18 @@
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart', ));
+
+if(!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },{
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+  }];
+}
+
+export function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -10,11 +24,27 @@ export function addToCart(productId) {
   });
 
   if (matchingItem){
-  matchingItem.quantity += 1;
-  } else {
-  cart.push({
-    productId: productId,
-    quantity:1
-  })
+    matchingItem.quantity += 1;
+    } else {
+    cart.push({
+      productId: productId,
+      quantity:1
+    })
   }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) =>{
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
 }
