686334a4d6502d57a1eea573c976a7e9929fbc4f
maluki65 <brayanmaluki@gmail.com>
2025-08-12 11:15:30 +0300

    Object oriented programming and classes

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
