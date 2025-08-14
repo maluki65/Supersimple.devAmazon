49ccbae4c96b576d1ed110b7dd48fb7edc4ad5ff
maluki65 <brayanmaluki@gmail.com>
2025-08-14 10:07:18 +0300

    makiing backend requests

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 //import '../data/cart-class.js';
+import '../data/backend-practise.js';
 
 renderOrderSummary();
 renderPaymentSummary();
